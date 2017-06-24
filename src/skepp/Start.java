ac24458bddf276fb63001b4735768e9fe1d4b863
Gunnar Lindroth <gunnar.lindroth@ericsson.com>
2017-06-24 16:06:51 +0200

    More game logic

--- src/skepp/Start.java
+++ src/skepp/Start.java
@@ -5,5 +5,6 @@ public final class Start {
   public static void main(String[] args) {
     System.out.println("Nu kör vi!");
     System.out.println("Jag skjuter på D3");
+    System.out.println("Mitt motdrag blir F9");
   }
 }
