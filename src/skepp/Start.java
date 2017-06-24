a6a167046e88952f267fb1e5a556ff5abfaf4288
Tobias Lindroth <tobias.lindroth@outlook.com>
2017-06-24 16:15:38 +0200

    Extend game logic

--- src/skepp/Start.java
+++ src/skepp/Start.java
@@ -2,10 +2,11 @@ package skepp;
 
 public final class Start {
 
-  public static void main(String[] args) {
-    System.out.println("Nu kör vi!");
-    System.out.println("Jag skjuter på D3");
-    System.out.println("Mitt motdrag blir F9");
-      System.out.println("Miss!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Nu kör vi!");
+        System.out.println("Jag skjuter på D3");
+        System.out.println("Mitt motdrag blir F9");
+        System.out.println("Miss!");
+        System.out.println("Miss igen!");
+    }
 }
