5d5b069651b41a28ab67e9fec66fc1d100836b82
Gunnar Lindroth <gunnar.lindroth@ericsson.com>
2017-06-24 22:07:55 +0200

    Fix incorrect commit, now you can add ships to board

--- src/skepp/Start.java
+++ src/skepp/Start.java
@@ -1,5 +1,6 @@
-<<<<<<< Upstream, based on branch 'master' of https://github.com/gunnarlindroth/skepp.git
 package skepp;
+
+import javax.swing.SwingUtilities;
 
 public final class Start {
 
@@ -15,28 +16,6 @@ public final class Start {
         
         Board board = new Board();
         
-        System.out.println(board);
-    }
-}
-=======
-package skepp;
-
-import javax.swing.SwingUtilities;
-
-public final class Start {
-
-    public static void main(String[] args) {
-        System.out.println("Nu kör vi!");
-        System.out.println("Jag skjuter på D3");
-        System.out.println("Mitt motdrag blir F9");
-        System.out.println("Miss!");
-        System.out.println("Miss igen!");
-        System.out.println("Träff!");
-                
-        System.out.println("\nNu måste vi göra ett bräde! Så här kanske?\n");
-        
-        Board board = new Board();
-        
         board.addShip(new Ship(ShipType.CRUISER, 2, 3, SwingUtilities.HORIZONTAL));
         
         board.addShip(new Ship(ShipType.DESTROYER, 5, 8, SwingUtilities.VERTICAL));
@@ -44,4 +23,3 @@ public final class Start {
         System.out.println(board);
     }
 }
->>>>>>> 8fcbb46 Add an attempt to add ships to board
