94db4e693845771579ed1fa2a4c5a97be763316d
Gunnar Lindroth <gunnar.lindroth@ericsson.com>
2017-06-24 16:06:07 +0200

    Format source code

--- src/skepp/Start.java
+++ src/skepp/Start.java
@@ -1,9 +1,9 @@
 package skepp;
 
 public final class Start {
-  
+
   public static void main(String[] args) {
     System.out.println("Nu kör vi!");
-      System.out.println("Jag skjuter på D3");
+    System.out.println("Jag skjuter på D3");
   }
 }
